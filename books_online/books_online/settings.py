aa01da9854aaf76c85508ead60e8c540ca761ccd
kostya <kuzminkostya2077@gmail.com>
2021-11-19 10:37:46 +0300

    change settings

--- books_online/books_online/settings.py
+++ books_online/books_online/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books_app.apps.BooksAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'books_online.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'books_and_authors',
+        'USER': 'kostya',
+        'PASSWORD': 'kuzya2077',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
