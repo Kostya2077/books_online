d8c6df8b333d0758e7aae71f53f7681189a9bdc1
kostya <kuzminkostya2077@gmail.com>
2021-11-19 19:22:42 +0300

    Implemented the administrative part a. CRUD operations for authors and books b. output a list of books with mandatory indication of the author's name in the list c. output a list of authors with indication of the number of books

--- books_online/books_app/admin.py
+++ books_online/books_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "count_books")
+    list_display_links = ("id", "name")
+    search_fields = ["count_books"]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "author")
+    list_display_links = ("id", "name", "author")
+    search_fields = ("author__name",)
+
