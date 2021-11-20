13f0f92cac076f7ca6f49c241c5fc8d77da55287
kostya <kuzminkostya2077@gmail.com>
2021-11-20 22:32:26 +0300

    update models (added field "id" as AutoField)

--- books_online/books_app/models.py
+++ books_online/books_app/models.py
@@ -1,4 +1,4 @@
-from django.db.models import CharField, ForeignKey, Model, CASCADE, IntegerField
+from django.db.models import CharField, ForeignKey, Model, CASCADE, IntegerField, F, AutoField
 from django.db.models import signals
 from django.dispatch import receiver
 
@@ -6,6 +6,7 @@ from django.dispatch import receiver
 class Author(Model):
     name = CharField(max_length=55, db_index=True, unique=True, verbose_name="Имя")
     count_books = IntegerField(default=0, verbose_name="Количество книг")
+
 
     def __repr__(self):
         return self.name
@@ -21,8 +22,9 @@ class Author(Model):
 
 
 class Book(Model):
-    name = CharField(max_length=255, db_index=True, null=False, unique=True, verbose_name="Название")
-    author = ForeignKey(Author, on_delete=CASCADE, null=False, verbose_name="Автор")
+    id = AutoField(primary_key=True)
+    name = CharField(max_length=255, db_index=True, unique=True, null=False, verbose_name="Название")
+    author = ForeignKey(Author, on_delete=CASCADE, null=False, verbose_name="Автор", to_field="name")
 
 
     def __repr__(self):
@@ -38,15 +40,22 @@ class Book(Model):
         verbose_name_plural = "Книги"
 
 
+"""СЧЁТЧИК КОЛИЧЕСТВА КНИГ АВТОРА"""
+
+"""при сохранении прибавляем 1 в поле count_books класса Author"""
 @receiver(signals.pre_save, sender=Book)
 def add_book_author(sender, instance, *args,**kwargs):
-    author = Author.objects.get(id=instance.author_id)
-    author.count_books = int(author.count_books) + 1
-    author.save()
+    Author.objects.filter(name=instance.author).update(count_books=F('count_books')+1)
+    """
+    если перед сохранением книга была у другого автора, убираем 1 книгу у того автора,
+    у которого она была
+    """
+    found_book = Book.objects.filter(name=instance.name)
+    if found_book:
+        Author.objects.filter(name=found_book[0].author).update(count_books=F('count_books') - 1)
 
 
+"""при удалении убираем 1 книгу из поля count_books класса Author"""
 @receiver(signals.pre_delete, sender=Book)
 def remove_book_author(sender, instance, *args,**kwargs):
-    author = Author.objects.get(id=instance.author_id)
-    author.count_books = int(author.count_books) - 1
-    author.save()
+    Author.objects.filter(name=instance.author).update(count_books=F('count_books')-1)
