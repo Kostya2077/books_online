3d6ed2315ceb63748ad1104e335013c7a5557b66
kostya <kuzminkostya2077@gmail.com>
2021-11-20 22:39:50 +0300

    added comments

--- books_online/books_app/admin.py
+++ books_online/books_app/admin.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 from .models import *
 
 
+""" Регистрация модели Author в панель админа """
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "count_books")
-    list_display_links = ("id", "name")
+    list_display = ["id", "name", "count_books"]
+    list_display_links = ["name"]
     search_fields = ["count_books"]
 
 
+""" Регистрация модели Book в панель админа """
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "author")
-    list_display_links = ("id", "name", "author")
-    search_fields = ("author__name",)
+    list_display = ["id", "name", "author"]
+    list_display_links = ["id", "name", "author"]
+    search_fields = ["author__name"]
 
