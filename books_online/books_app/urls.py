e984211d2d3b1b5bb0d3f4da404046de613f4ff9
kostya <kuzminkostya2077@gmail.com>
2021-11-20 22:45:55 +0300

    added api urls

--- books_online/books_app/urls.py
+++ books_online/books_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('api/v1/books/list', get_books_list),
+    path('api/v1/books/<int:pk>', get_book_by_id),
+    path('api/v1/books/update', update_book),
+    path('api/v1/books/delete/<int:pk>', delete_book_by_id),
 ]
